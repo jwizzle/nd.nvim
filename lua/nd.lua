b0ce7aaad7d3218bf30dcfc2889a143a44230eb6
Jwizzle <info@hossel.net>
2023-08-02 21:23:52 +0200

    Using wget in the end anyways

--- lua/nd.lua
+++ lua/nd.lua
@@ -1,5 +1,4 @@
 local utils = require("nd/utils")
--- local http = require("socket.http")
 
 --- Notitiedoos, a zettelkasten plugin for neovim, leveraging telescope and zettelgo.
 local nd = {
@@ -29,21 +28,13 @@ local nd = {
 vim.api.nvim_create_user_command(
     'NdInstall',
     function()
-      print(debug.getinfo(1).source)
       local datadir = debug.getinfo(1).source:match("@?(.*/)")
-      print(datadir)
---       nd.localbinary = datadir .. "zettelgo"
---       local zettelgo_release = "https://github.com/jwizzle/zettelgo/releases/download/v1.0.4/zettelgo-linux-amd64"
--- 
---       local body, code = http.request(zettelgo_release)
---       if not body then error(code) end
---       local f = assert(io.open(nd.localbinary, 'wb'))
---       f:write(body)
---       f:close()
--- 
---       utils.os_capture("chmod -v +x " .. nd.localbinary)
--- 
---       print("Downloaded zettelgo to: " .. nd.localbinary)
+      nd.localbinary = datadir .. "zettelgo"
+      local zettelgo_release = "https://github.com/jwizzle/zettelgo/releases/download/v1.0.4/zettelgo-linux-amd64"
+      utils.os_capture("wget -nv -O " .. nd.localbinary .. " " .. zettelgo_release .. " 2>&1")
+      utils.os_capture("chmod -v +x " .. nd.localbinary)
+
+      print("Downloaded zettelgo to: " .. nd.localbinary)
     end,
     {}
 )
